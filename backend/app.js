63bf0d41400bf47e2d0a73590481f90b9ca4ab88
ptmff <ptmff@yandex.ru>
2025-10-07 07:17:14 +0300

    Added frontend directory

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ const { logger, logAction } = require('./utils/logger');
 app.use(securityHeaders);
 app.use(xssProtection);
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || '*',
+  origin: '*',
   credentials: true,
 }));
 
